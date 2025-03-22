4445cac35b3c4ebc28fa04b6e6a116448a1a0090
Stergianni <stella.orfanidis@hotmail.com>
2025-03-22 12:26:26 +0100

    Upload backend-nodejs-capstone

---
+++ IBM Back-end JavaScript Developer/backend-nodejs-capstone/secondChance-backend/util/import-mongo/index.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient
+const fs = require('fs')
+const path = require('path') // Import path module
+
+// MongoDB connection URL with authentication options
+const url = `${process.env.MONGO_URL}`
+// Use path.join() to build the path to your file
+const filename = path.join(__dirname, 'secondChanceItems.json')
+const dbName = 'secondChance'
+const collectionName = 'secondChanceItems'
+
+// Notice you have to load the array of items into the data object
+const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
+
+// Connect to database and insert data into the collection
+async function loadData () {
+  const client = new MongoClient(url)
+
+  try {
+    // Connect to the MongoDB client
+    await client.connect()
+    console.log('Connected successfully to server')
+
+    // Database will be created if it does not exist
+    const db = client.db(dbName)
+
+    // Collection will be created if it does not exist
+    const collection = db.collection(collectionName)
+    const cursor = await collection.find({})
+    const documents = await cursor.toArray()
+
+    if (documents.length === 0) { // Use strict equality ===
+      // Insert data into the collection
+      const insertResult = await collection.insertMany(data)
+      console.log('Inserted documents:', insertResult.insertedCount)
+    } else {
+      console.log('Items already exist in DB')
+    }
+  } catch (err) {
+    console.error(err)
+  } finally {
+    // Close the connection
+    await client.close()
+  }
+}
+
+loadData()
+
+module.exports = {
+  loadData
+}
